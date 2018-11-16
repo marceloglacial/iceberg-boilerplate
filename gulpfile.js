b9f7c7f6843bf741b997b06c2c8bb9704b4f042f
Marcelo Glacial <glacial@gmail.com>
2018-11-16 17:08:53 -0500

    Add sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const del = require('del');
 const download = require('gulp-download');
 const decompress = require('gulp-decompress');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
 
 // Configre Project
 // =======================================
@@ -17,8 +19,25 @@ gulp.task('reset', () =>
     del(['tmp', 'download', 'server', 'node_modules'])
 );
 
+// Minfy Files
+// =======================================
+function style() {
+    return (
+        gulp
+            .src("src/assets/sass/*.*")
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(gulp.dest("src/assets/css"))
+    );
+}
+exports.style = style;
 
-// Install Wordpress
+function sassWatch(){
+    gulp.watch('src/assets/sass/*.*', style)
+}
+exports.watch = sassWatch
+
+// Install Wordpress and start serve
 // =======================================
 gulp.task('wp-download', () =>
     download(wpUrl + wpVersion)
@@ -32,10 +51,13 @@ gulp.task('wp-unzip', () =>
 gulp.task('wp-clean', () =>
     del(['tmp', 'download'])
 );
+gulp.task('wp-watch-clean', () =>
+    del('server/wp-content/themes/' + wpThemeName)
+);
 gulp.task('wp-copy', wpCopy);
 function wpCopy(done) {
-    gulp.src('./src/*.*')
-        .pipe(gulp.dest('./server/wp-content/themes/' + wpThemeName))
+    gulp.src('src/**/*.*')
+        .pipe(gulp.dest('server/wp-content/themes/' + wpThemeName))
         done();
 };
 
@@ -44,7 +66,8 @@ function start() {
     browserSync.init({
         proxy: localServer + wpThemeName + "/server/"
     })
-    gulp.watch("src/**/*.*").on('change', gulp.series(['wp-copy', browserSync.reload]))
+    gulp.watch("src/**/*.*").on('change', gulp.series(['wp-watch-clean', 'wp-copy', browserSync.reload]))
+    gulp.watch('src/assets/sass/*.*', style)
 };
 
 gulp.task('install', gulp.series('wp-clean', 'wp-download', 'wp-unzip', 'wp-copy', 'wp-clean', 'start'));
