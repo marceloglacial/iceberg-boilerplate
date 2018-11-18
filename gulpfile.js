5afa14c42ea4675a6ee68a2c9cbefc9f820475fb
Marcelo Glacial <glacial@gmail.com>
2018-11-18 12:18:40 -0500

    Add minify scripts

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Paths
 const paths = {
@@ -22,8 +23,10 @@ const paths = {
         server: 'server',
         tmp: 'tmp'
     },
-    root: {
-        all: './src/**/*.*'
+    project: {
+        all: './src/**/*.*',
+        src: './src/',
+        dist: './dist/'
     },
     styles: {
         src: './src/assets/css/src/*.*',
@@ -39,21 +42,23 @@ const paths = {
     }
 };
 
+
 // Download Wordpress
 function wpDownload() {
     return (
         download(paths.wordpress.url + '/' + paths.wordpress.version)
         .pipe(gulp.dest(paths.wordpress.tmp))
-    ) 
+    )
 }
 exports.wpDownload = wpDownload
 
 // Decompress Wordpress and add to server folder
 function wpUnzip() {
     return (
-        gulp.src(paths.wordpress.tmp + '/*.{tar,tar.bz2,tar.gz,zip}')
-        .pipe(decompress({strip: 1}))
-        .pipe(gulp.dest(paths.wordpress.server))
+        gulp
+            .src(paths.wordpress.tmp + '/*.{tar,tar.bz2,tar.gz,zip}')
+            .pipe(decompress({ strip: 1 }))
+            .pipe(gulp.dest(paths.wordpress.server))
     )
 }
 exports.wpUnzip = wpUnzip
@@ -61,8 +66,9 @@ exports.wpUnzip = wpUnzip
 // Copy file from work folder to server folder
 function wpCopy() {
     return (
-        gulp.src(paths.root.all)
-        .pipe(gulp.dest(paths.wordpress.server + '/wp-content/themes/' + paths.wordpress.themeName))
+        gulp
+            .src(paths.project.all)
+            .pipe(gulp.dest(paths.wordpress.server + '/wp-content/themes/' + paths.wordpress.themeName))
     )
 }
 exports.wpCopy = wpCopy
@@ -90,7 +96,6 @@ function styles() {
             .pipe(postcss([autoprefixer(), cssnano()]))
             .pipe(sourcemaps.write())
             .pipe(gulp.dest(paths.styles.dest))
-            .pipe(browserSync.stream())
     );
 }
 exports.styles = styles
@@ -98,43 +103,61 @@ exports.styles = styles
 // Minify JavaScript
 function scripts() {
     return (
-        gulp.src(paths.scripts.src, {
-            sourcemaps: true
-        })
-        .pipe(uglify())
-        .pipe(concat('main.min.js'))
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(browserSync.stream())
-        );
+        gulp
+            .src(paths.scripts.src, {
+                sourcemaps: true
+            })
+            .pipe(uglify())
+            .pipe(concat('main.min.js'))
+            .pipe(gulp.dest(paths.scripts.dest))
+    );
 }
 exports.scripts = scripts
 
 // Minify Images
 function images() {
-    return gulp.src('src/assets/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/assets/img'))
-};
-
-// Build Files
-function build() {
     return (
-        gulp.src(paths.root.all)
-        .pipe(gulp.dest(paths.wordpress.server + '/wp-content/themes/' + paths.wordpress.themeName))
-        .pipe(browserSync.stream())
+        gulp
+            .src('src/assets/img/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('dist/assets/img'))
+    ) 
+};
+exports.images = images
+
+// Minify HTML 
+function html() {
+    return (
+        gulp
+            .src(paths.project.src + '/**/*.{html, html, php')
+            .pipe(htmlmin({ collapseWhitespace: true, removeComments: true, minifyCSS: true, minifyJS: true }))
+            .pipe(gulp.dest(paths.project.dist))
     )
 }
+exports.html = html
+
+
+// BrowserSync with new file
+function live() {
+    return (
+        gulp
+            .src(paths.project.all)
+            .pipe(gulp.dest(paths.wordpress.server + '/wp-content/themes/' + paths.wordpress.themeName))
+            .pipe(browserSync.stream())
+    )
+}
+exports.live = live
+
 
 // Watch Files
 function watch() {
-    gulp.watch(paths.scripts.src, scripts)
-    gulp.watch(paths.styles.src, styles)
-    gulp.watch(paths.root.all, build)
+    gulp.watch(paths.styles.src, styles) // sass
+    gulp.watch(paths.project.all, live) // copy files to server and refresh browser
 }
 exports.watch = watch
 
 // Start server
-function start(){
+function start() {
     browserSync.init({
         proxy: paths.wordpress.proxy + '/' + paths.wordpress.themeName + '/' + paths.wordpress.server
     });
@@ -143,9 +166,8 @@ function start(){
 exports.start = start
 
 
-
 // !!!! DANGER !!!! 
 // =======================================
-const reset = gulp.series(wpClean, () => del([paths.wordpress.tmp, 'node_modules']))
+const reset = gulp.series(wpClean, () => del(['node_modules', paths.project.dist, paths.wordpress.tmp, paths.wordpress.server]))
 gulp.task('reset', reset)
 // =======================================
